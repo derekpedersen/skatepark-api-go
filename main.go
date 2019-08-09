62d52076175504e2587e523f1580472e5297ed76
Derek Pedersen <derekpedersen.com@gmail.com>
2019-08-08 20:26:00 -0700

    Added an explicit allowed list

--- main.go
+++ main.go
@@ -14,17 +14,34 @@ func main() {
 	log.SetLevel(log.DebugLevel) // TODO: make this a flag
 	log.Infof("log level: %v", log.GetLevel())
 
+	skateparkRouter, err := appcfg.NewSkateparkAPIRouter()
+	if err != nil {
+		log.Fatalf("failed to create skateparkRouter: %v", err)
+	}
+
+	// setup cors option
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{
+			"http://localhost",
+			"http://localhost:8080",
+			"http://skatepark-api",
+			"http://skatepark-api:8080",
+			"http://skatepark-api-go-service",
+			"http://skatepark-api-go-service:8080",
+			"http://celebrityskateboards.com",
+			"https://celebrityskateboards.com",
+		},
+		AllowCredentials: true,
+		// Enable Debugging for testing, consider disabling in production
+		//Debug: true,
+	})
+
 	var wg sync.WaitGroup
-
-	skateparkRouter, _ := appcfg.NewSkateparkAPIRouter()
-
-	// Use default options
-	handler := cors.Default().Handler(skateparkRouter)
 
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		log.Fatal(http.ListenAndServe(":8080", handler))
+		log.Fatal(http.ListenAndServe(":8080", c.Handler(skateparkRouter)))
 	}()
 
 	wg.Wait()
