d7c91ec3d30c6a65e84eea84b403235842cad4ca
Derek Pedersen <derekpedersen.com@gmail.com>
2019-10-21 20:41:22 -0700

    Starting to work on adding some scripts

---
+++ .tools/scripts/build.sh
@@ -0,0 +1,9 @@
+
+# clean out existing build artifacts
+rm -rf bin
+
+# gather dependencies
+dep ensure -v
+
+# build binary
+go build -o bin/skatepark-api-go
---
+++ .tools/scripts/unit-test.sh
@@ -0,0 +1,12 @@
+
+# execute golang tests
+go test ./... -covermode=count -v -coverprofile cp.out
+
+# go get cobertura lib
+go get github.com/t-yuki/gocover-cobertura
+
+# generate output files
+go tool cover -html=cp.out -o cp.html && gocover-cobertura < cp.out > cp.xml
+
+# code coverage print
+go tool cover -func=cp.out
