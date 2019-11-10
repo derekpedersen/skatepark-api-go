8436f45ee85d0ffd03c0860bddcff3a3937a69d1
Derek Pedersen <derekpedersen.com@gmail.com>
2019-11-09 16:56:43 -0800

    Cross over to 50% test coverage!

--- controller/health_test.go
+++ controller/health_test.go
@@ -85,3 +85,117 @@ func TestGetAliveMessage(t *testing.T) {
 		})
 	}
 }
+
+func TestGetReadyMessage(t *testing.T) {
+	// Arrange
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	svc := mock.NewMockHealthService(ctrl)
+	api := controller.NewHealthAPIController(svc)
+
+	type args struct {
+		method string
+		url    string
+		params map[string]string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		statuscode int
+	}{
+		{
+			name: "GetReadyMessage: 200",
+			args: args{
+				method: http.MethodGet,
+				url:    "/ready",
+			},
+			statuscode: 200,
+		},
+	}
+
+	msg := model.HealthMessage{
+		Message: "Howdy",
+	}
+
+	gomock.InOrder(
+		// GetAliveMessage: 200
+		svc.EXPECT().GetReadyMessage().Return(&msg),
+	)
+
+	// Act
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, err := http.NewRequest(tt.args.method, tt.args.url, nil)
+			if err != nil {
+				t.Fatalf("encountered an unexpected error: %v", err)
+			}
+
+			rr := httptest.NewRecorder()
+			req = mux.SetURLVars(req, tt.args.params)
+			handler := http.HandlerFunc(api.GetReadyMessage)
+			handler.ServeHTTP(rr, req)
+
+			// Assert
+			if rr.Code != tt.statuscode {
+				t.Errorf("unexpected status code: have %v, want %v", rr.Code, tt.statuscode)
+			}
+		})
+	}
+}
+
+func TestGetHealthyMessage(t *testing.T) {
+	// Arrange
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	svc := mock.NewMockHealthService(ctrl)
+	api := controller.NewHealthAPIController(svc)
+
+	type args struct {
+		method string
+		url    string
+		params map[string]string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		statuscode int
+	}{
+		{
+			name: "GetHealthyMessage: 200",
+			args: args{
+				method: http.MethodGet,
+				url:    "/healthy",
+			},
+			statuscode: 200,
+		},
+	}
+
+	msg := model.HealthMessage{
+		Message: "Howdy",
+	}
+
+	gomock.InOrder(
+		// GetHealthyMessage: 200
+		svc.EXPECT().GetHealthyMessage().Return(&msg),
+	)
+
+	// Act
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, err := http.NewRequest(tt.args.method, tt.args.url, nil)
+			if err != nil {
+				t.Fatalf("encountered an unexpected error: %v", err)
+			}
+
+			rr := httptest.NewRecorder()
+			req = mux.SetURLVars(req, tt.args.params)
+			handler := http.HandlerFunc(api.GetHealthyMessage)
+			handler.ServeHTTP(rr, req)
+
+			// Assert
+			if rr.Code != tt.statuscode {
+				t.Errorf("unexpected status code: have %v, want %v", rr.Code, tt.statuscode)
+			}
+		})
+	}
+}
