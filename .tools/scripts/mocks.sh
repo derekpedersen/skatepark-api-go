b82c88c6fdf18c5b620a7254d6c322c602cc9c47
Derek Pedersen <derekpedersen.com@gmail.com>
2022-05-31 11:56:06 -0700

    working on updating dep mocks

--- .tools/scripts/mocks.sh
+++ .tools/scripts/mocks.sh
@@ -17,5 +17,5 @@ mockgen -source=service/health.go -destination=mock/mock_health_service.go -pack
 mockgen -source=controller/skateparks.go -destination=mock/mock_skateparks_controller.go -package=mock
 mockgen -source=controller/health.go -destination=mock/mock_health_controller.go -package=mock
 
-# external deps
-mockgen -source=vendor/github.com/derekpedersen/imgur-go/service/album.go -destination=mock/mock_album_service.go -package=mock
+# # external deps
+# mockgen -source=vendor/github.com/derekpedersen/imgur-go/service/album.go -destination=mock/mock_album_service.go -package=mock
